b128fe9131f60ba918481ae57d41c0dd843bd669
Miguelecf <mxcf98@gmail.com>
2025-06-07 12:46:05 -0300

    Implementando CasaDepto

--- models/CasaDepto.js
+++ models/CasaDepto.js
@@ -1,3 +1,28 @@
-class CasaDepto extends Alojamiento{
+class CasaDepto extends Alojamiento {
+    
+    constructor(direccion, precioPorNoche, cantidadAmbientes) {
+        super(direccion);
+        if (!precioPorNoche || precioPorNoche <= 0) {
+            throw new Error("El precio por noche es obligatorio y debe ser mayor a cero");
+        }
+        if (!cantidadAmbientes || cantidadAmbientes <= 0) {
+            throw new Error("La cantidad de ambientes es obligatoria y debe ser mayor a cero");
+        }
+        this.precioPorNoche = precioPorNoche; // Precio por noche del alojamiento
+        this.cantidadAmbientes = cantidadAmbientes; // Cantidad de ambientes del alojamiento
+    }
+
+    getPrecio() {
+        let precioPorNoche;
+        if (this.cantidadAmbientes === 1) {
+            precioPorNoche = 15000;
+        } else if (this.cantidadAmbientes <= 4) {
+            precioPorNoche = 30000;
+        } else {
+            precioPorNoche = 50000;
+        }
+
+        return precioPorNoche * this.noches;
+    }
 
 }
